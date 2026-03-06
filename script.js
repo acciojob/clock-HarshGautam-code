3f38fd9d933d25e82dd2fe373e8c374fdd3c4c73
HarshGautam-code <149058537+HarshGautam-code@users.noreply.github.com>
2026-03-06 15:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 
 function updateTimer() {
-
     const timer = document.getElementById("timer");
     const now = new Date();
 
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-
-    // add leading zero
-    if (hours < 10) hours = "0" + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-
-    const time = hours + ":" + minutes + ":" + seconds;
-
-    const date = now.toDateString();
-
-    timer.innerHTML = time + "<br>" + date;
+    timer.textContent = now.toLocaleString();
 }
 
 updateTimer();
